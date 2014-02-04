4e726cf6c007993cd3b64e454630cb06e17d21a8
Iouri Khramtsov <ioukhr@gmail.com>
2014-02-03 19:28:14 -0500

    More translations to English.

--- DBDiff.Schema.SQLServer2005/Compare/CompareTables.cs
+++ DBDiff.Schema.SQLServer2005/Compare/CompareTables.cs
@@ -6,7 +6,7 @@ namespace DBDiff.Schema.SQLServer.Generates.Compare
 {
     internal class CompareTables:CompareBase<Table>
     {
-        protected override void DoUpdate<Root>(SchemaList<Table, Root> CamposOrigen, Table node, DiffsConfig config = null)
+        protected override void DoUpdate<Root>(SchemaList<Table, Root> originFields, Table node, DiffsConfig config = null)
         {
             if (config == null)
             {
@@ -15,37 +15,37 @@ namespace DBDiff.Schema.SQLServer.Generates.Compare
 
             if (node.Status != Enums.ObjectStatusType.DropStatus)
             {
-                Table tablaOriginal = CamposOrigen[node.FullName];
-                tablaOriginal.OriginalTable = (Table)CamposOrigen[node.FullName].Clone((Database)tablaOriginal.Parent);
-                if (config.columns) (new CompareColumns()).GenerateDiferences<Table>(tablaOriginal.Columns, node.Columns);
-                if (config.constraints) (new CompareConstraints()).GenerateDiferences<Table>(tablaOriginal.Constraints, node.Constraints);
-                if (config.indexes) (new CompareIndexes()).GenerateDiferences<Table>(tablaOriginal.Indexes, node.Indexes);
-                if (config.table_options) (new CompareTablesOptions()).GenerateDiferences<Table>(tablaOriginal.Options, node.Options);
-                if (config.triggers) (new CompareTriggers()).GenerateDiferences<Table>(tablaOriginal.Triggers, node.Triggers);
-                if (config.clr_triggers) (new CompareCLRTriggers()).GenerateDiferences<Table>(tablaOriginal.CLRTriggers, node.CLRTriggers);
-                if (config.full_text_indexes) (new CompareFullTextIndex()).GenerateDiferences<Table>(tablaOriginal.FullTextIndex, node.FullTextIndex);
+                Table originTable = originFields[node.FullName];
+                originTable.OriginalTable = (Table)originFields[node.FullName].Clone((Database)originTable.Parent);
+                if (config.columns) (new CompareColumns()).GenerateDiferences<Table>(originTable.Columns, node.Columns);
+                if (config.constraints) (new CompareConstraints()).GenerateDiferences<Table>(originTable.Constraints, node.Constraints);
+                if (config.indexes) (new CompareIndexes()).GenerateDiferences<Table>(originTable.Indexes, node.Indexes);
+                if (config.table_options) (new CompareTablesOptions()).GenerateDiferences<Table>(originTable.Options, node.Options);
+                if (config.triggers) (new CompareTriggers()).GenerateDiferences<Table>(originTable.Triggers, node.Triggers);
+                if (config.clr_triggers) (new CompareCLRTriggers()).GenerateDiferences<Table>(originTable.CLRTriggers, node.CLRTriggers);
+                if (config.full_text_indexes) (new CompareFullTextIndex()).GenerateDiferences<Table>(originTable.FullTextIndex, node.FullTextIndex);
                 
                 if (config.file_groups
-                    && (!Table.CompareFileGroup(tablaOriginal, node)))
+                    && (!Table.CompareFileGroup(originTable, node)))
                 {
-                    tablaOriginal.FileGroup = node.FileGroup;
+                    originTable.FileGroup = node.FileGroup;
                     /* This only applies to heap tables, the rest does the field in the clustered index filegroup */
-                    if (!tablaOriginal.HasClusteredIndex)
-                        tablaOriginal.Status = Enums.ObjectStatusType.RebuildStatus;
+                    if (!originTable.HasClusteredIndex)
+                        originTable.Status = Enums.ObjectStatusType.RebuildStatus;
                 }
                 
                 if (config.file_groups
-                    && (!Table.CompareFileGroupText(tablaOriginal, node)))
+                    && (!Table.CompareFileGroupText(originTable, node)))
                 {
-                    tablaOriginal.FileGroupText = node.FileGroupText;
-                    tablaOriginal.Status = Enums.ObjectStatusType.RebuildStatus;
+                    originTable.FileGroupText = node.FileGroupText;
+                    originTable.Status = Enums.ObjectStatusType.RebuildStatus;
                 }
                 
-                if (node.HasChangeTracking != tablaOriginal.HasChangeTracking)
+                if (node.HasChangeTracking != originTable.HasChangeTracking)
                 {
-                    tablaOriginal.HasChangeTracking = node.HasChangeTracking;
-                    tablaOriginal.HasChangeTrackingTrackColumn = node.HasChangeTrackingTrackColumn;
-                    tablaOriginal.Status += (int)Enums.ObjectStatusType.DisabledStatus;
+                    originTable.HasChangeTracking = node.HasChangeTracking;
+                    originTable.HasChangeTrackingTrackColumn = node.HasChangeTrackingTrackColumn;
+                    originTable.Status += (int)Enums.ObjectStatusType.DisabledStatus;
                 }
             }
         }
